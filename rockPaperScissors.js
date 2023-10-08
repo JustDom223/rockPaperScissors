4d0e6e45d48c6648c8d87c61627a8ada713e67dc
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-08 12:49:20 +0200

    completed the game loop I had trouble getting the final score but ended it with an if statement that reset the scores and the round count

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -28,7 +28,7 @@ function determineGameOutcome(playerChoice, computerChoice) {
 }
 
 // return the appropriate roundOutcome
-function compareHand(result, playerChoice, computerChoice){
+function generateOutcomeMessage(result, playerChoice, computerChoice){
     if (result === 'lose'){
         return `Your hand is ${playerChoice} and the computer\'s hand is ${computerChoice}... You Lose!`
     }else if(result === 'win'){
@@ -38,71 +38,100 @@ function compareHand(result, playerChoice, computerChoice){
     };
 }
 
+
 // the gameplay loop
 let playerScore = 0
 let computerScore = 0
 let totalRounds = 5
+let playerChoice = '';
+let roundOutcome = '';
+let roundResult = '';
 // Manipulate the buttons to decide what the player picks
 document.addEventListener("DOMContentLoaded", function(){
     const buttonContainerElement = document.getElementById("buttonContainer");
     const outcomeTextElement = document.getElementById("outcome")
     const playerScoreElement = document.getElementById("playerScore")
     const computerScoreElement = document.getElementById("computerScore")
+    
+    function updateScores(roundOutcome){
+        if (roundOutcome === "win"){
+            playerScore++;
+            playerScoreElement.textContent = `Player\'s score: ${playerScore}`
+            totalRounds--;
+        }else if (roundOutcome === "lose"){
+            computerScore ++;
+            computerScoreElement.textContent = `Computer\'s score: ${computerScore}`
+            totalRounds--;
+        }
+    }
+    
+
+    function gameOver(totalRounds, playerScore, computerScore){
+        if(totalRounds <= 0){
+            if(playerScore > computerScore){
+                outcomeTextElement.textContent = `HEY! Your score is ${playerScore} and the computer 
+                only has ${computerScore}. YOU WIN!!!`
+            }else{
+                outcomeTextElement.textContent = `HEY! Your score is ${playerScore} and the computer 
+                has ${computerScore}. Better luck next time!!!`
+            }
+            // outcomeTextElement.textContent = roundResult
+            
+        }
+        
+    }
+
 
     buttonContainerElement.addEventListener("click", function(event){
         if (event.target.tagName === 'BUTTON') {
-            let playerChoice = '';
             let computerChoice = getRandomChoice(possibleChoices);
-            let roundOutcome = ''
-            let roundResult = ''
+
 
             switch(event.target.id) {
                 case "rockButton": 
                 playerChoice = 'rock'
                 roundOutcome = determineGameOutcome(playerChoice, computerChoice)
-                roundResult = compareHand(roundOutcome, playerChoice, computerChoice)
+                roundResult = generateOutcomeMessage(roundOutcome, playerChoice, computerChoice)
                 outcomeTextElement.textContent = roundResult
-                if (roundOutcome === "win"){
-                    playerScore++;
-                    playerScoreElement.textContent = playerScore
-                }else if (roundOutcome === "lose"){
-                    computerScore++;
-                    computerScoreElement.textContent = computerScore
-                };
+                updateScores(roundOutcome)               
                 break
                 
                 
                 case "paperButton": 
                 playerChoice = 'paper'
                 roundOutcome = determineGameOutcome(playerChoice, computerChoice)
-                roundResult = compareHand(roundOutcome, playerChoice, computerChoice)
+                roundResult = generateOutcomeMessage(roundOutcome, playerChoice, computerChoice)
                 outcomeTextElement.textContent = roundResult
-                if (roundOutcome === "win"){
-                    playerScore++;
-                    playerScoreElement.textContent = playerScore
-                }else if (roundOutcome === "lose"){
-                    computerScore ++;
-                    computerScoreElement.textContent = computerScore
-                }
+                updateScores(roundOutcome)   
                 break
                 
                 
                 case "scissorsButton": 
                 playerChoice = 'scissors'
                 roundOutcome = determineGameOutcome(playerChoice, computerChoice)
-                roundResult = compareHand(roundOutcome, playerChoice, computerChoice)
+                roundResult = generateOutcomeMessage(roundOutcome, playerChoice, computerChoice)
                 outcomeTextElement.textContent = roundResult
-                if (roundOutcome === "win"){
-                    playerScore++;
-                    playerScoreElement.textContent = playerScore
-                }else if (roundOutcome === "lose"){
-                    computerScore ++;
-                    computerScoreElement.textContent = computerScore
-                }
+                updateScores(roundOutcome)
                 break
                 
                 
             }
+        }
+        if(totalRounds <= 0){
+            if(playerScore > computerScore){
+                outcomeTextElement.textContent = `HEY! Your score is ${playerScore} and the computer 
+                only has ${computerScore}. YOU WIN!!!`
+            }else{
+                outcomeTextElement.textContent = `HEY! Your score is ${playerScore} and the computer 
+                only has ${computerScore}. Better luck next time!!!`
+            }
+            computerScore = 0
+            playerScore = 0
+            totalRounds = 5
+            computerScoreElement.textContent = `Computer\'s score: ${computerScore}`
+            playerScoreElement.textContent = `Player\'s score: ${playerScore}`
+            // outcomeTextElement.textContent = roundResult
+            
         }
 })
 
